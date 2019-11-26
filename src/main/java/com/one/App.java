2984fb136861f5297b6bb79bee58a08495baf21c
aakash123 khurana <=>
2019-11-26 07:47:57 +0530

    Changes in App.java

--- src/main/java/com/one/App.java
+++ src/main/java/com/one/App.java
@@ -11,26 +11,35 @@ import com.aliasi.tokenizer.EnglishStopTokenizerFactory;
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
 import com.aliasi.tokenizer.Tokenizer;
 import com.aliasi.tokenizer.TokenizerFactory;
-public class App 
-{
-    public static void main( String[] args )
-    {
-      //  System.out.println( "Hello World!" );
-        
-        String sentence = "";
-        sentence  = sentence.replaceAll("\\p{Punct}",""); 
-        TokenizerFactory tokenizerFactory =
-        	    IndoEuropeanTokenizerFactory.INSTANCE;
-        	tokenizerFactory = new EnglishStopTokenizerFactory(tokenizerFactory);
-        	Tokenizer tokenizer = tokenizerFactory.tokenizer(
-        		    sentence.toCharArray(), 0, sentence.length());
-        	int count  = 0; 
-        	Set<String> set = new HashSet<String>();
-        	for (String token : tokenizer) {
-        	    set.add(token);
-        	    count++;
-        	}
-        	System.err.println(count+" " +sentence.split(" ").length);
-        	System.err.println(set.size());
-    }
+
+public class App {
+	public static void main(String[] args) {
+		// System.out.println( "Hello World!" );
+
+		String paragraph = "";
+		String heading = "";
+		paragraph = paragraph.replaceAll("\\p{Punct}", "");
+		TokenizerFactory tokenizerFactory = IndoEuropeanTokenizerFactory.INSTANCE;
+		tokenizerFactory = new EnglishStopTokenizerFactory(tokenizerFactory);
+		Tokenizer tokenizer = tokenizerFactory.tokenizer(paragraph.toCharArray(), 0, paragraph.length());
+		Set<String> setWords = new HashSet<String>();
+		for (String token : tokenizer) {
+			setWords.add(token);
+		}
+
+		String[] headingWords = heading.split(" ");
+		int flag = 0 ;
+		for(String s:headingWords){
+			if(setWords.contains(s)){
+				System.out.println("Heading is correct");
+				flag++;
+				break;
+			}
+		}
+		if(flag==0){
+			System.out.println("Heading is incorrect");
+		}
+		
+
+	}
 }
